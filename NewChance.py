b9ab8bfd1bbaa45e7d962bd07634d861414f3a9a
kbukhtin <buxtin.kir@yandex.ru>
2022-05-29 23:45:21 +0300

    test 1

---
+++ NewChance.py
@@ -0,0 +1,27 @@
+#Task №1
+'''
+x = int(input())
+y = int(input())
+print(x * 60 + y)
+'''
+#Task №2
+'''
+Y = int(input())
+X = Y // 60
+y = Y % 60
+print(X)
+print(y)
+'''
+#Task №3
+'''
+x = int(input())
+h = int(input())
+m = int(input())
+hour = (h * 60 + m + x) // 60
+minutes = (h * 60 + m + x) % 60
+print(hour)
+print(minutes)
+'''
+#Task №4
+
+544+4545
