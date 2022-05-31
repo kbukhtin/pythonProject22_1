61873e54e0ebd1aa08dae0a3b664fcf2303f7f35
kbukhtin <buxtin.kir@yandex.ru>
2022-05-31 20:10:14 +0300

    Общие задание по курсу

--- Test/NewChance.py
+++ Test/NewChance.py
@@ -21,5 +21,35 @@ hour = (h * 60 + m + x) // 60
 minutes = (h * 60 + m + x) % 60
 print(hour)
 print(minutes)
-
+'''
 #Task №4
+'''
+A = int(input())
+B = int(input())
+H = int(input())
+
+if A <= H <= B:
+    print('Это нормально')
+elif A > H:
+    print('Недосып')
+elif B < H:
+    print('Пересып')
+'''
+#Task №5
+'''
+year = int(input())
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print('Високосный')
+else:
+    print('Обычный')
+'''
+#Task №6
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+p = (a + b + c) / 2
+S = (p * (p - a) * (p - b) * (p -c)) ** 0.5
+print(S)
+'''
+#Task №7
