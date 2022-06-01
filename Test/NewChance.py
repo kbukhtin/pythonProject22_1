25a94d1f6309c1605c7b4f590a829eadfaec1389
kbukhtin <buxtin.kir@yandex.ru>
2022-06-01 21:19:48 +0300

    full 1 блок

--- Test/NewChance.py
+++ Test/NewChance.py
@@ -1,3 +1,4 @@
+# Операторы. Переменные. Условия.
 #Task №1
 '''
 x = int(input())
@@ -82,4 +83,87 @@ elif _operator == "div" and operand2 != 0:
 elif _operator == "/" or _operator == "mod" or _operator == "div" and operand2 == 0:
     print("Деление на 0!")
 '''
-#Task №9
+# Test №9
+'''
+_type = input()
+p = 3.14
+if _type == 'треугольник':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    p = (1 / 2) * (a + b + c)
+    S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+if _type == 'прямоугольник':
+    a = int(input())
+    b = int(input())
+    S = a * b
+if _type == 'круг':
+    r = int(input())
+    S = p * (r ** 2)
+print(S)
+'''
+# Test №10
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+if (a >= b and a >= c) and (b >= c):
+    print(a)
+    print(c)
+    print(b)
+elif (a >= b and a >= c) and (c >= b):
+    print(a)
+    print(b)
+    print(c)
+elif (b >= a and b >= c) and (a >= c):
+    print(b)
+    print(c)
+    print(a)
+elif (b >= a and b >= c) and (c >= a):
+    print(b)
+    print(a)
+    print(c)
+elif (c >= a and c >= b) and (b >= a):
+    print(c)
+    print(a)
+    print(b)
+elif (c >= a and c >= b) and (a >= b):
+    print(c)
+    print(b)
+    print(a)
+
+ALT:
+a,b,c=int(input ()),int(input ()),int(input ())
+if a<b: a,b=b,a
+if a<c: a,c=c,a
+if b<c: c,b=b,c
+print (a,c,b,sep='\n')
+'''
+# Test №11
+'''
+a = int(input())
+b = 'программист'
+if a % 10 == 1 and a % 100 != 11:
+    с = str(a) + ' ' + b
+elif (a % 10 == 2 and a % 100 != 12) or (a % 10 == 3 and a % 100 != 13) or (a % 10 == 4 and a % 100 != 14):
+    с = str(a) + ' ' + b + 'а'
+else:
+    с = str(a) + ' ' + b + 'ов'
+print(с)
+'''
+# Test №12
+'''
+num = int(input())
+a = num // 100000
+b = (num // 10000) % 10
+c = (num //1000) % 10
+d = (num // 100) % 10
+e = (num // 10) % 10
+f = num % 10
+if a + b + c == d + e + f:
+    print('Счастливый')
+else:
+    print('Обычный')
+'''
+#Циклы. Строки. Списки
+# Test №12
